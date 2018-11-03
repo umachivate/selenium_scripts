8dc7bde5731c6c4dd78f88c33f61c3fd56abfe68
Uma <https://github.com/umachivate/selenium_scripts.git>
2018-11-03 21:25:18 +0530

    committing file

---
+++ src/pkg_testng_examples/New_class.java
@@ -0,0 +1,11 @@
+package pkg_testng_examples;
+
+public class New_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello");
+	}
+
+}
